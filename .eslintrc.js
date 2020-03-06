79e32e6cf00e243fd160cad9b50a4ef5649117c3
Halmakari <kosha.1997@bk.ru>
2020-03-06 19:58:33 +0300

    Change airbnb config to airbnb-base

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
         "es6": true,
         "node": true
     },
-    "extends": ["airbnb","plugin:prettier/recommended"],
+    "extends": ["airbnb-base","plugin:prettier/recommended"],
     "parserOptions": {
         "ecmaVersion": 2018,
         "sourceType": "module"
--- src/knexfile.js
+++ src/knexfile.js
@@ -24,4 +24,4 @@ module.exports = {
       max: 10,
     },
   },
-}
+};
